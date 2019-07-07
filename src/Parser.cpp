05326367c4aa6ed64a17085376a053162a57bb79
DomMarcone <dammarcone@yahoo.com>
2019-07-06 21:46:35 -0400

    Seperated test/MathNodesTest.cpp and src/Parser.cpp into two files to extend Parser.cpp while keeping MathNodesTest.cpp simple.

---
+++ src/Parser.cpp
@@ -0,0 +1,81 @@
+//Parser.cpp
+#include "MathNodes.hpp"
+#include "MathTreeBuilder.hpp"
+
+#include <stdio.h>
+#include <time.h>
+
+#include <math.h>
+
+#define MAX_BUFFER_SIZE 128
+
+float help_func();
+float time_func();
+float exit_func();
+
+
+int main(int argc, char **args){
+	char temp[MAX_BUFFER_SIZE];
+	float pi = 3.14159265359;
+	float e = 2.7182818284;
+	
+	float count = 0;
+	
+	MathTreeBuilder mtb;
+	
+	mtb.addVariable(&pi, "pi");
+	mtb.addVariable(&e, "e");
+	mtb.addVariable(&count, "count");
+	
+	mtb.addFunction(time_func,"time");
+	mtb.addFunction(help_func,"help");
+	mtb.addFunction(exit_func,"exit");
+	
+	mtb.addFunction(sinf,"sin");
+	mtb.addFunction(cosf,"cos");
+	mtb.addFunction(tanf,"tan");
+	
+	mtb.addFunction(asinf,"asin");
+	mtb.addFunction(acosf,"acos");
+	mtb.addFunction(atanf,"atan");
+		
+	//mtb.addFunction(sqrtf,"sqrt"); //removed. Please use "^0.5"
+	
+	while(true){
+		MathNode *mn;
+		printf(">");
+		gets(temp);
+		mn = mtb.parse(temp);
+		printf(" %f\n\n", mn->getValue());
+		
+		delete mn;
+		count += 1.f;
+	}
+	
+	return 0;
+}
+
+
+float time_func(){ return clock()/1000.f; }
+
+float help_func(){
+	printf(" Mathmatical parser which works recursively\n");
+	printf(" on substrings. This is only a test.\n");
+	printf(" Currently supported functions :\n");
+	printf("\thelp()\t-prints this\n");
+	printf("\ttime()\t-returns time in seconds\n");
+	printf("\texit()\t-exits the parser\n");
+	printf("\tsin(theta)\n");
+	printf("\tcos(theta)\n");
+	printf("\ttan(theta)\n");
+	printf("\tasin(theta)\n");
+	printf("\tacos(theta)\n");
+	printf("\tatan(theta)\n");
+	printf("\n");
+	return 1.f;
+}
+
+float exit_func(){
+	exit(0);
+	return 0.f;
+}
--- test/MathNodesTest.cpp
+++ test/MathNodesTest.cpp
@@ -9,7 +9,6 @@
 
 #define MAX_BUFFER_SIZE 128
 
-float help_func();
 float time_func();
 float exit_func();
 
@@ -17,15 +16,12 @@ float exit_func();
 int main(int argc, char **args){
 	char temp[MAX_BUFFER_SIZE];
 	float pi = 3.14159265359;
-	float e = 2.7182818284;
 	
 	MathTreeBuilder mtb;
 	
 	mtb.addVariable(&pi, "pi");
-	mtb.addVariable(&e, "e");
 	
 	mtb.addFunction(time_func,"time");
-	mtb.addFunction(help_func,"help");
 	mtb.addFunction(exit_func,"exit");
 	
 	//addFunction is overloaded to handle both:
@@ -34,12 +30,6 @@ int main(int argc, char **args){
 	mtb.addFunction(sinf,"sin");
 	mtb.addFunction(cosf,"cos");
 	mtb.addFunction(tanf,"tan");
-	
-	mtb.addFunction(asinf,"asin");
-	mtb.addFunction(acosf,"acos");
-	mtb.addFunction(atanf,"atan");
-		
-	//mtb.addFunction(sqrtf,"sqrt"); //removed. Please use "^0.5"
 	
 	while(true){
 		MathNode *mn;
@@ -55,24 +45,6 @@ int main(int argc, char **args){
 
 
 float time_func(){ return clock()/1000.f; }
-
-float help_func(){
-	printf(" Mathmatical parser which works recursively\n");
-	printf(" on substrings. This is only a test.\n");
-	printf(" Currently supported functions :\n");
-	printf("\thelp()\t-prints this\n");
-	printf("\ttime()\t-returns time in seconds\n");
-	printf("\texit()\t-exits the parser\n");
-	//printf("\tsqrt(n)\n");
-	printf("\tsin(theta)\n");
-	printf("\tcos(theta)\n");
-	printf("\ttan(theta)\n");
-	printf("\tasin(theta)\n");
-	printf("\tacos(theta)\n");
-	printf("\tatan(theta)\n");
-	printf("\n");
-	return 1.f;
-}
 
 float exit_func(){
 	exit(0);
